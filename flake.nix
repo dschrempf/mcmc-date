7f08125bcb7259c8410443046cef97c7a7469c27
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-08-27 14:19:33 +0200

    nix flake

--- flake.nix
+++ flake.nix
@@ -1,53 +1,66 @@
 {
-  description = "Evolution environment";
+  description = "Date phylogenetic trees with Mcmc";
 
-  inputs.nixpkgs.url = "path:/home/dominik/Nix/Nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.dschrempf-nur.url = "path:/home/dominik/Shared/config/nixos/nur";
+  inputs.dschrempf-nur.url = "github:dschrempf/nur-packages";
   inputs.dschrempf-nur.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, dschrempf-nur }:
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.mcmc.url = "github:dschrempf/mcmc";
+  inputs.mcmc.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, dschrempf-nur, flake-utils, mcmc, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (
       system:
         let
+          mcmc-overlay = (
+            selfn: supern: {
+              haskellPackages = supern.haskellPackages.override {
+                overrides = selfh: superh:
+                  {
+                    mcmc = mcmc.defaultPackage.${system};
+                  };
+              };
+            }
+          );
+          overlays = [ mcmc-overlay ];
           pkgs = import nixpkgs {
-            inherit system;
-            config = { allowUnfree = true; };
+            inherit system overlays;
           };
           dschrempf = import dschrempf-nur {
             inherit pkgs;
           };
-          evolHaskellEnv = pkgs.haskell.packages.ghc8104.ghcWithPackages
-            (
-              haskellPackages: with haskellPackages; [
-                # ELynx library.
-                elynx
-                slynx
-                tlynx
-              ]
-            );
+          mcmc-date-package =
+            let
+              p = pkgs.haskellPackages.callCabal2nix "mcmc-date" ./. rec {};
+            in
+              pkgs.haskell.lib.doBenchmark p;
         in
           {
-            devShell = pkgs.mkShell {
-              shellHook = let
-                scripts = ./scripts;
-              in
-                ''
-                  export PATH="${scripts}:$PATH"
-                '';
-              packages =
-                with dschrempf;
-                [
-                  # GHC with libraries.
-                  evolHaskellEnv
+            defaultPackage = mcmc-date-package;
 
-                  # Tools.
-                  beast2
-                  figtree
-                  iqtree2
-                  phylobayes
-                  tracer
-                ];
+            devShell = pkgs.haskellPackages.shellFor {
+              shellHook =
+                let
+                  scripts = ./scripts;
+                in
+                  ''
+                    export PATH="${scripts}:$PATH"
+                  '';
+              packages = _: [ mcmc-date-package ];
+              buildInputs = [
+                dschrempf.beast2
+                dschrempf.figtree
+                dschrempf.iqtree2
+                dschrempf.phylobayes
+                dschrempf.tracer
+                pkgs.haskellPackages.elynx
+                pkgs.haskellPackages.slynx
+                pkgs.haskellPackages.tlynx
+              ];
+              doBenchmark = true;
             };
           }
     );
