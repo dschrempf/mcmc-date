d38bb8b5943dc762ce1eef1b9832bafaeeee410c
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-08-27 15:22:42 +0200

    use unstable repos

--- flake.nix
+++ flake.nix
@@ -1,8 +1,17 @@
 {
   description = "Date phylogenetic trees with Mcmc";
 
+  inputs.circular.url = "github:dschrempf/circular";
+  inputs.circular.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.dirichlet.url = "github:dschrempf/dirichlet";
+  inputs.dirichlet.inputs.nixpkgs.follows = "nixpkgs";
+
   inputs.dschrempf-nur.url = "github:dschrempf/nur-packages";
   inputs.dschrempf-nur.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.elynx.url = "github:dschrempf/elynx";
+  inputs.elynx.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
@@ -11,57 +20,75 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, dschrempf-nur, flake-utils, mcmc, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          mcmc-overlay = (
-            selfn: supern: {
-              haskellPackages = supern.haskellPackages.override {
-                overrides = selfh: superh:
-                  {
-                    mcmc = mcmc.defaultPackage.${system};
-                  };
+  inputs.pava.url = "github:dschrempf/pava";
+  inputs.pava.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs =
+    { self
+    , circular
+    , dirichlet
+    , dschrempf-nur
+    , elynx
+    , flake-utils
+    , mcmc
+    , nixpkgs
+    , pava
+    }:
+      flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            haskell-overlay = (
+              selfn: supern: {
+                haskellPackages = supern.haskellPackages.override {
+                  overrides = selfh: superh:
+                    {
+                      circular = circular.defaultPackage.${system};
+                      dirichlet = dirichlet.defaultPackage.${system};
+                      mcmc = mcmc.defaultPackage.${system};
+                      pava = pava.defaultPackage.${system};
+                    } // elynx.packages.${system};
+                };
+              }
+            );
+            overlays = [ haskell-overlay ];
+            pkgs = import nixpkgs {
+              inherit system overlays;
+            };
+            dschrempf = import dschrempf-nur {
+              inherit pkgs;
+            };
+            mcmc-date-package =
+              let
+                p = pkgs.haskellPackages.callCabal2nix "mcmc-date" ./. rec {};
+              in
+                pkgs.haskell.lib.doBenchmark p;
+          in
+            {
+              defaultPackage = mcmc-date-package;
+
+              devShell = pkgs.haskellPackages.shellFor {
+                shellHook =
+                  let
+                    scripts = ./scripts;
+                  in
+                    ''
+                      export PATH="${scripts}:$PATH"
+                    '';
+                packages = _: [ mcmc-date-package ];
+                buildInputs = with pkgs; [
+                  dschrempf.beast2
+                  dschrempf.figtree
+                  dschrempf.iqtree2
+                  dschrempf.phylobayes
+                  dschrempf.tracer
+                  haskellPackages.cabal-install
+                  haskellPackages.elynx
+                  haskellPackages.haskell-language-server
+                  haskellPackages.slynx
+                  haskellPackages.tlynx
+                ];
+                doBenchmark = true;
               };
             }
-          );
-          overlays = [ mcmc-overlay ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          dschrempf = import dschrempf-nur {
-            inherit pkgs;
-          };
-          mcmc-date-package =
-            let
-              p = pkgs.haskellPackages.callCabal2nix "mcmc-date" ./. rec {};
-            in
-              pkgs.haskell.lib.doBenchmark p;
-        in
-          {
-            defaultPackage = mcmc-date-package;
-
-            devShell = pkgs.haskellPackages.shellFor {
-              shellHook =
-                let
-                  scripts = ./scripts;
-                in
-                  ''
-                    export PATH="${scripts}:$PATH"
-                  '';
-              packages = _: [ mcmc-date-package ];
-              buildInputs = [
-                dschrempf.beast2
-                dschrempf.figtree
-                dschrempf.iqtree2
-                dschrempf.phylobayes
-                dschrempf.tracer
-                pkgs.haskellPackages.elynx
-                pkgs.haskellPackages.slynx
-                pkgs.haskellPackages.tlynx
-              ];
-              doBenchmark = true;
-            };
-          }
-    );
+      );
 }
