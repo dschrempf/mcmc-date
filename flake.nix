fd36c02ff20ba791416f8b506a02a8b9fb5289fd
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-11-10 13:49:00 +0100

    update

--- flake.nix
+++ flake.nix
@@ -2,39 +2,22 @@
   description = "Date phylogenetic trees with Mcmc";
 
   inputs.circular.url = "github:dschrempf/circular";
-  inputs.circular.inputs.flake-utils.follows = "flake-utils";
-  inputs.circular.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.covariance.url = "github:dschrempf/covariance";
-  inputs.covariance.inputs.flake-utils.follows = "flake-utils";
-  inputs.covariance.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.dirichlet.url = "github:dschrempf/dirichlet";
-  inputs.dirichlet.inputs.flake-utils.follows = "flake-utils";
-  inputs.dirichlet.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.dschrempf-nur.url = "github:dschrempf/nur-packages";
-  inputs.dschrempf-nur.inputs.flake-utils.follows = "flake-utils";
-  inputs.dschrempf-nur.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.elynx.url = "github:dschrempf/elynx";
-  inputs.elynx.inputs.flake-utils.follows = "flake-utils";
-  inputs.elynx.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.mcmc.url = "github:dschrempf/mcmc";
-  inputs.mcmc.inputs.circular.follows = "circular";
-  inputs.mcmc.inputs.dirichlet.follows = "dirichlet";
-  inputs.mcmc.inputs.flake-utils.follows = "flake-utils";
-  inputs.mcmc.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.mcmc.inputs.pava.follows = "pava";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   inputs.pava.url = "github:dschrempf/pava";
-  inputs.pava.inputs.flake-utils.follows = "flake-utils";
-  inputs.pava.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs =
     { self
