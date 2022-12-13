d2ddab16f40178d216b9ece14048a1e92fe09317
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-14 00:10:25 +0100

    greatly simplify inputs by composing overlays

--- flake.nix
+++ flake.nix
@@ -1,18 +1,6 @@
 {
   description = "Date phylogenetic trees with Mcmc";
 
-  inputs.circular.url = "github:dschrempf/circular";
-  inputs.circular.inputs.flake-utils.follows = "flake-utils";
-  inputs.circular.inputs.nixpkgs.follows = "nixpkgs";
-
-  inputs.covariance.url = "github:dschrempf/covariance";
-  inputs.covariance.inputs.flake-utils.follows = "flake-utils";
-  inputs.covariance.inputs.nixpkgs.follows = "nixpkgs";
-
-  inputs.dirichlet.url = "github:dschrempf/dirichlet";
-  inputs.dirichlet.inputs.flake-utils.follows = "flake-utils";
-  inputs.dirichlet.inputs.nixpkgs.follows = "nixpkgs";
-
   inputs.dschrempf-nur.url = "github:dschrempf/nur-packages";
   inputs.dschrempf-nur.inputs.flake-utils.follows = "flake-utils";
   inputs.dschrempf-nur.inputs.nixpkgs.follows = "nixpkgs";
@@ -25,31 +13,17 @@
 
   inputs.mcmc.url = "github:dschrempf/mcmc";
   # inputs.mcmc.url = "path:/home/dominik/Shared/haskell/mcmc";
-  inputs.mcmc.inputs.circular.follows = "circular";
-  inputs.mcmc.inputs.covariance.follows = "covariance";
-  inputs.mcmc.inputs.dirichlet.follows = "dirichlet";
-  inputs.mcmc.inputs.flake-utils.follows = "flake-utils";
-  inputs.mcmc.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.mcmc.inputs.pava.follows = "pava";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   # inputs.nixpkgs.url = "path:/home/dominik/Nix/Nixpkgs";
 
-  inputs.pava.url = "github:dschrempf/pava";
-  inputs.pava.inputs.flake-utils.follows = "flake-utils";
-  inputs.pava.inputs.nixpkgs.follows = "nixpkgs";
-
   outputs =
     { self
-    , circular
-    , covariance
-    , dirichlet
     , dschrempf-nur
     , elynx
     , flake-utils
     , mcmc
     , nixpkgs
-    , pava
     }:
     let
       theseHpkgNames = [
@@ -71,11 +45,7 @@
             inherit system;
             overlays = [
               hOverlay
-              circular.overlays.default
-              covariance.overlays.default
-              dirichlet.overlays.default
               mcmc.overlays.default
-              pava.overlays.default
               elynx.overlays.default
             ];
           };
